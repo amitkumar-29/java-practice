77804fbf9f4abc27da0f8a28364dc2244ef63049
amitkumar-29 <tcsamitkumar29@gmai.com>
2021-08-10 10:04:41 +0000

    EncahnceForLoop

--- src/com/tcs/entity/ArraysDemo.java
+++ src/com/tcs/entity/ArraysDemo.java
@@ -11,6 +11,19 @@ public static void main(String [] args) {
 	for(int i=0;i<no.length;i++) {
 		System.out.println(no[i]);
 		
+		
 	}
+	EnchancedForLoop();
+}
+private static void EnchancedForLoop() {
+	int numbers[]=new int[10];
+	numbers[0]=1;
+	numbers[1]=2;
+	for(int number:numbers) {
+		if(number%2==0)
+		System.out.println(number);
+
+	}
+	
 }
 }
